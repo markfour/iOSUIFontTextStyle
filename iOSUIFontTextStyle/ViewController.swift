dbc8bbfdb67e2f8f2c89e67552e92088ee2ef162
markfour <axhfaro@gmail.com>
2018-08-30 14:05:07 +0900

    Intialize

---
+++ iOSUIFontTextStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSUIFontTextStyle
+//
+//  Created by kazumi hayashida on 2018/08/30.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
