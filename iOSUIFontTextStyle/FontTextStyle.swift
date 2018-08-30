da453a06c3dce044f75ca98efcb6034105244496
markfour <axhfaro@gmail.com>
2018-08-30 14:13:22 +0900

    Add FontTextStyle

---
+++ iOSUIFontTextStyle/FontTextStyle.swift
@@ -0,0 +1,9 @@
+//
+//  FontTextStyle.swift
+//  iOSUIFontTextStyle
+//
+//  Created by kazumi hayashida on 2018/08/30.
+//  Copyright © 2018年 kazumi hayashida. All rights reserved.
+//
+
+import Foundation
